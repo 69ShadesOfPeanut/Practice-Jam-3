b4729546cec0113cfda030ec3d1e594abf18b205
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2024-05-02 01:48:32 +1000

    Update Buttons.gd

--- Scenes/Buttons.gd
+++ Scenes/Buttons.gd
@@ -2,6 +2,8 @@
 extends HBoxContainer
 
 # Vars
+var GoodScore : int
+var BadScore : int
 # Nodes
 @onready var ScreenNode : Control = get_node("%Screen")
 @onready var ScreenText : Label = ScreenNode.get_node("%Text")
