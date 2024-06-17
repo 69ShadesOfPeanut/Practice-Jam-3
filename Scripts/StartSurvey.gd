af09d9d1b5aa55c9d7e01c18cccb830ecfbed822
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2024-06-18 00:14:09 +1000

    Start survey confirmation
    asks for confirmation before going to the survey scene

--- Scripts/StartSurvey.gd
+++ Scripts/StartSurvey.gd
@@ -2,5 +2,14 @@
 extends WindowNode
 
 
-func _ready():
+
+# Called when user presses yes
+# Changes the scene
+func YesButtonPressed():
 	get_tree().change_scene_to_file("res://Scenes/office.tscn")
+
+
+# Called when the user presses no
+# Closes the window
+func NoButtonPressed():
+	queue_free()
