5364c5372dfadfce0d5983672d151ebff9c0f5b2
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2024-06-28 02:30:03 +1000

    Basic question app
    app that you ask a question and it gives a response. has some scripted questions that it responds to

---
+++ Scripts/Questions.gd
@@ -0,0 +1,44 @@
+# Script for handling question app.
+# Gives random answers to questions unless its a specific coded question
+extends WindowNode
+
+# Vars
+@export var DefaultAnswers : Array[String]
+@export var ScriptedQuestions : Dictionary
+# Nodes
+@onready var QuestionEnterNode : LineEdit = get_node("%QuestionEnter")
+@onready var CurrentScene : Control = get_tree().current_scene
+@onready var OpenApps : Control = CurrentScene.get_node("%OpenApps")
+# Resources
+const AnswerScene = preload("res://Scenes/ComputerApps/Answer.tscn")
+
+
+# Called when players submit a question
+func SubmitButtonPressed():
+	# Return if question is empty
+	if QuestionEnterNode.text.strip_edges(true, true).is_empty():
+		print("Question is empty")
+		QuestionEnterNode.clear()
+		return
+	
+	# Make an instance of the answer scene and set properties
+	var AnswerSceneInstance = AnswerScene.instantiate()
+	AnswerSceneInstance.set_position(Vector2(300, 300))
+	AnswerSceneInstance.set_title("Answer to: " + QuestionEnterNode.text)
+	
+	
+	
+	# Goes through scripted questions and sees if question asked matches
+	# if it matches, display the dictionary value / result.
+	var Question = QuestionEnterNode.text.rstrip("?").to_lower()
+	var IsScriptedQuestion = false
+	for key in ScriptedQuestions:
+		if Question == key.to_lower():
+			AnswerSceneInstance.get_node("AnswerLabel").set_text(ScriptedQuestions[key])
+			IsScriptedQuestion = true
+		elif IsScriptedQuestion == false:
+			AnswerSceneInstance.get_node("AnswerLabel").set_text(DefaultAnswers.pick_random())
+	
+	# Clear text enter and add instance to scene
+	QuestionEnterNode.clear()
+	OpenApps.add_child(AnswerSceneInstance)
