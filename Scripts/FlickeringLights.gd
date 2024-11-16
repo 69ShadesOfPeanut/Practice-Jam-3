467ea2ed7731489e147f41cdf8477795a95fa344
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2024-11-16 14:12:29 +1100

    Flicker alt time
    Flickering lights now have the ability to have a second defined time before they turn back on.
    
    This makes the time between turning off and on different if specified in the node variables

--- Scripts/FlickeringLights.gd
+++ Scripts/FlickeringLights.gd
@@ -6,7 +6,15 @@ extends Light3D
 @export var MinTime : float
 ## The maximum time between light flickers
 @export var MaxTime : float
-
+## Should the min/max time act as timer between lights flickering off.
+## Normally the min/max time counts as both light on and off time
+@export var FlickerAlt : bool = false
+## The minimum time before light turns back on.
+## Only use if flicker alt is enabled
+@export var FlickerMinTime : float
+## The maximum time before light turns back on.
+## Only use if flicker alt is enabled
+@export var FlickerMaxTime : float
 
 # Start function
 func _ready():
@@ -20,4 +28,14 @@ func Flicker():
 	#print("Flicker time is: " + str(FlickerTime))
 	await get_tree().create_timer(FlickerTime).timeout
 	visible =! visible
+	
+	# if flicker alt is enabled, wait an amount of time determined by flicker alt time
+	if FlickerAlt == true:
+		randomize()
+		var FlickerTimeAlt = randf_range(FlickerMinTime, FlickerMaxTime)
+		#print("Flicker alt time is: " + str(FlickerTimeAlt))
+		await get_tree().create_timer(FlickerTimeAlt).timeout
+		visible =! visible
+	
+	# Repeat the loop
 	Flicker()
