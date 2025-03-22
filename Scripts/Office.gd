429a5ed652a415952feea87508e10a17e789b510
Peanut <63982558+69ShadesOfPeanut@users.noreply.github.com>
2025-03-22 18:43:11 +1100

    Ambience
    Changed the background ambience and added the pc humming sound as a 3D sound in the room

--- Scripts/Office.gd
+++ Scripts/Office.gd
@@ -3,10 +3,18 @@
 extends Node
 
 # Vars
+const AMBIENCE_D_19_LOOP = preload("res://Sounds/ambience_d19_loop.ogg")
 const OFFICE_COMPUTER_PC_FAN_NOISE_LOOP = preload("res://Sounds/Office Computer PC Fan Noise Loop.wav")
+# Nodes
+@onready var PCAmbienceNode : AudioStreamPlayer3D = get_node("%PCAmbienceNode")
 
 
 func _ready():
-	Ambience.set_stream(OFFICE_COMPUTER_PC_FAN_NOISE_LOOP)
+	# Load background ambience
+	Ambience.set_stream(AMBIENCE_D_19_LOOP)
 	Ambience.play()
-	print("Ambience loaded up")
+	
+	# Load PC ambience
+	PCAmbienceNode.set_stream(OFFICE_COMPUTER_PC_FAN_NOISE_LOOP)
+	PCAmbienceNode.play()
+	print("Office loaded")
